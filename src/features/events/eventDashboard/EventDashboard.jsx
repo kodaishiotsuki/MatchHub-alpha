03e6a07ffda269e77b035754e0d851750f609e85
塩月康大 <0711dr5@gmail.com>
2022-03-16 12:52:26 +0900

    入力フォーム作成

--- src/features/events/eventDashboard/EventDashboard.jsx
+++ src/features/events/eventDashboard/EventDashboard.jsx
@@ -6,13 +6,24 @@ import { sampleData } from "../../../app/api/sampleData";
 
 const EventDashboard = ({ formOpen, setFormOpen }) => {
   const [events, setEvents] = useState(sampleData);
+  //フォームが送信された時に呼び出す関数
+  function handleCreateEvent(event) {
+    setEvents([...events, event]);
+}
+
   return (
     <Grid>
       <Grid.Column width={10}>
         <EventList events={events} />
       </Grid.Column>
       <Grid.Column width={6}>
-        {formOpen && <EventForm setFormOpen={setFormOpen} />}
+        {formOpen && (
+          <EventForm
+            setFormOpen={setFormOpen}
+            setEvents={setEvents}
+            createEvent={handleCreateEvent}
+          />
+        )}
       </Grid.Column>
     </Grid>
   );
--- src/features/events/eventForm/EventForm.jsx
+++ src/features/events/eventForm/EventForm.jsx
@@ -1,28 +1,92 @@
-import React from "react";
+import cuid from "cuid";
+import React, { useState } from "react";
 import { Button, Form, Header, Segment } from "semantic-ui-react";
 
-export default function EventForm({ setFormOpen }) {
+export default function EventForm({ setFormOpen, setEvents, createEvent }) {
+  //inputフォーム
+  const initialValues = {
+    title: "",
+    category: "",
+    description: "",
+    city: "",
+    venue: "",
+    date: "",
+  };
+  const [values, setValues] = useState(initialValues);
+  //送信ボタン
+  function handleFormSubmit() {
+    createEvent({
+      ...values,
+      id: cuid(),
+      hostedBy: "Bob",
+      attendees: [],
+      hostPhotoURL: "/assets/user.png",
+    });
+    setFormOpen(false);
+  }
+  //onChangeイベント e.target.valueをオブジェクトにまとめる
+  function handleInputChange(e) {
+    const { name, value } = e.target;
+    setValues({ ...values, [name]: value });
+  }
+
   return (
     <Segment clearing>
-      <Form>
+      <Form onSubmit={handleFormSubmit}>
         <Header content='Create new event' />
         <Form.Field>
-          <input type='text' placeholder='Event title' />
+          <input
+            type='text'
+            name='title'
+            placeholder='Event title'
+            value={values.title}
+            onChange={(e) => handleInputChange(e)}
+          />
         </Form.Field>
         <Form.Field>
-          <input type='text' placeholder='Category' />
+          <input
+            type='text'
+            name='category'
+            placeholder='Category'
+            value={values.category}
+            onChange={(e) => handleInputChange(e)}
+          />
         </Form.Field>
         <Form.Field>
-          <input type='text' placeholder='Description' />
+          <input
+            type='text'
+            name='description'
+            placeholder='Description'
+            value={values.description}
+            onChange={(e) => handleInputChange(e)}
+          />
         </Form.Field>
         <Form.Field>
-          <input type='text' placeholder='City' />
+          <input
+            type='text'
+            name='city'
+            placeholder='City'
+            value={values.city}
+            onChange={(e) => handleInputChange(e)}
+          />
         </Form.Field>
         <Form.Field>
-          <input type='text' placeholder='Venue' />
+          <input
+            type='text'
+            name='venue'
+            placeholder='Venue'
+            value={values.venue}
+            onChange={(e) => handleInputChange(e)}
+          />
         </Form.Field>
         <Form.Field>
-          <input type='date' placeholder='Date' />
+          <input
+            type='date'
+            name='date'
+            placeholder='Date'
+            value={values.date}
+            onChange={(e) => handleInputChange(e)}
+          />
         </Form.Field>
         <Button type='submit' floated='right' positive content='Submit' />
         <Button
