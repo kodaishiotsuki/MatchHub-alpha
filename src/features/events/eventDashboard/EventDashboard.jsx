0cacbc2bb34e903607f6c6d9dc059073b1e02d50
塩月康大 <0711dr5@gmail.com>
2022-03-20 10:43:57 +0900

    redux thunk処理

--- src/features/events/eventDashboard/EventDashboard.jsx
+++ src/features/events/eventDashboard/EventDashboard.jsx
@@ -2,17 +2,28 @@ import React from "react";
 import { Grid } from "semantic-ui-react";
 import EventList from "./EventList";
 import { useSelector } from "react-redux";
+import EventListItemPlaceholder from "./EventListItemPlaceholder";
+import EventFilter from "./EventFilter";
 
 const EventDashboard = () => {
   const { events } = useSelector((state) => state.event);
 
+  //ローディング
+  const { loading } = useSelector((state) => state.async);
+  
+
   return (
     <Grid>
       <Grid.Column width={10}>
+        {loading && (
+          <>
+            <EventListItemPlaceholder />
+          </>
+        )}
         <EventList events={events} />
       </Grid.Column>
       <Grid.Column width={6}>
-        <h2>Event Filters</h2>
+        <EventFilter />
       </Grid.Column>
     </Grid>
   );
