86f0b5f5fdf5596a22465bdd4b030dfe95d0e015
塩月康大 <0711dr5@gmail.com>
2022-03-20 20:36:36 +0900

    firebase apikey 削除

--- src/app/config/firebase.js
+++ src/app/config/firebase.js
@@ -6,12 +6,6 @@ import "firebase/compat/auth";
 import "firebase/compat/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBMZBpaWx82DGDu6MrJfxWG45ohWaRJaTs",
-  authDomain: "matchhub-af483.firebaseapp.com",
-  projectId: "matchhub-af483",
-  storageBucket: "matchhub-af483.appspot.com",
-  messagingSenderId: "458263535246",
-  appId: "1:458263535246:web:460d66a872d2ec03c0b3df",
 };
 
 firebase.initializeApp(firebaseConfig);
